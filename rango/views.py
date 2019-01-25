0472655b89de85d88f8533815a5a4f5ef45f3303
Yatin Sharma <2330306s@student.gla.ac.uk>
2019-01-25 17:28:09 +0000

    first commit

--- rango/views.py
+++ rango/views.py
@@ -15,7 +15,8 @@ def index(request):
     # Place the list in our context_dict dictionary
     # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')
+    context_dict = {'categories': category_list, 'pages': page_list}
     # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
 
@@ -46,4 +47,5 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
+
 
